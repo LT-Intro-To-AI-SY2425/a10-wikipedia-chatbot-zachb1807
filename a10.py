eeb6145a7851ea250c479fbe2f511485f68c8efe
Zach B <56662213+zachb1807@users.noreply.github.com>
2025-05-21 21:31:11 -0500

    finish all code

--- a10.py
+++ a10.py
@@ -7,6 +7,8 @@ from nltk.tree import Tree
 from match import match
 from typing import List, Callable, Tuple, Any, Match
 
+previous_name = ""
+
 
 def get_page_html(title: str) -> str:
     """Gets html of a wikipedia page
@@ -128,6 +130,66 @@ def birth_date(matches: List[str]) -> List[str]:
     """
     return [get_birth_date(" ".join(matches))]
 
+def incumbency_start(matches: List[str]) -> List[str]:
+    """Returns incumbency start date of named person in matches
+
+    Args:
+        matches - match from pattern of person's name to find incumbency start date of
+
+    Returns:
+        incumbency start date of named person
+    """
+    return [get_incumbency_start(" ".join(matches))]
+
+def incumbency_start_year(matches: List[str]) -> List[str]:
+    """Returns incumbency start year of named person in matches
+
+    Args:
+        matches - match from pattern of person's name to find incumbency start year of
+
+    Returns:
+        incumbency start year of named person
+    """
+    date = get_incumbency_start(" ".join(matches))
+    return [date.split(",")[1].strip() if date else "No answers"]
+
+def incumbency_end(matches: List[str]) -> List[str]:
+    """Returns incumbency end date of named person in matches
+
+    Args:
+        matches - match from pattern of person's name to find incumbency end date of
+
+    Returns:
+        incumbency end date of named person
+    """
+    return [get_incumbency_end(" ".join(matches))]
+
+def incumbency_end_year(matches: List[str]) -> List[str]:
+    """Returns incumbency end year of named person in matches
+
+    Args:
+        matches - match from pattern of person's name to find incumbency end year of
+
+    Returns:
+        incumbency end year of named person
+    """
+    date = get_incumbency_end(" ".join(matches))
+    return [date.split(",")[1].strip() if date else "No answers"]
+
+def number(matches: List[str]) -> List[str]:
+    """Returns presidential number of named person in matches
+
+    Args:
+        matches - match from pattern of person's name to find presidential number of
+
+    Returns:
+        presidential number of named person
+    """
+    results = get_number(" ".join(matches))
+    if len(results) > 1:
+        return [results[0] + " & " + results[1]]
+    return results
+
 
 def polar_radius(matches: List[str]) -> List[str]:
     """Returns polar radius of planet in matches
@@ -139,6 +201,45 @@ def polar_radius(matches: List[str]) -> List[str]:
         polar radius of planet
     """
     return [get_polar_radius(matches[0])]
+
+
+def get_incumbency_start(name: str) -> str:
+    infobox_text = clean_text(get_first_infobox_text(get_page_html(name)))
+    pattern = r"President of the United StatesIn office+(?P<officestart>[A-Za-z]+\s+\d{1,2},\s+\d{4})"
+    error_text = "Page infobox has no incumbency start information"
+    try:
+        match = get_match(infobox_text, pattern, error_text)
+        return match.group("officestart")
+    except AttributeError:
+        newpattern = r"IncumbentAssumed office\s+(?P<officestart>[A-Za-z]+\s+\d{1,2},\s+\d{4})"
+        match = get_match(infobox_text, newpattern, error_text)
+        return match.group("officestart")
+
+
+def get_incumbency_end(name: str) -> str:
+    infobox_text = clean_text(get_first_infobox_text(get_page_html(name)))
+    pattern = r"President of the United StatesIn office+([A-Za-z]+\s+\d{1,2},\s+\d{4})\s+(?P<officeend>[A-Za-z]+\s+\d{1,2},\s+\d{4})"
+
+    error_text = "Page infobox has no incumbency end information"
+    try:
+        match = get_match(infobox_text, pattern, error_text)
+        return match.group("officeend")
+    except AttributeError:
+        newpattern = r"IncumbentAssumed office\s+(?P<officestart>[A-Za-z]+\s+\d{1,2},\s+\d{4})"
+        match = get_match(infobox_text, newpattern, error_text)
+        match.group("officestart")
+        return "Current incumbent"
+
+def get_number(name: str) -> List[str]:
+    infobox_text = clean_text(get_first_infobox_text(get_page_html(name)))
+    pattern = r"(\d{2})(?=(?:st|nd|rd|th)\b(?: & \b\d+(?:st|nd|rd|th)\b)? President of the United States)"
+
+    error_text = "Page infobox has no presidential number information"
+    matches = re.findall(pattern, infobox_text)
+    if not matches:
+        raise AttributeError("Page infobox has no presidential number information")
+    return matches
+
 
 
 # dummy argument is ignored and doesn't matter
@@ -154,8 +255,21 @@ Action = Callable[[List[str]], List[Any]]
 # The pattern-action list for the natural language query system. It must be declared
 # here, after all of the function definitions
 pa_list: List[Tuple[Pattern, Action]] = [
-    ("when was % born".split(), birth_date),
-    ("what is the polar radius of %".split(), polar_radius),
+    ("when did % take office".split(), incumbency_start),
+    ("when did % become president".split(), incumbency_start),
+    ("what year did % take office".split(), incumbency_start_year),
+    ("what year did % become president".split(), incumbency_start_year),
+    ("when did % begin his presidency".split(), incumbency_start),
+    ("what year did % begin his presidency".split(), incumbency_start_year),
+    ("when did % leave office".split(), incumbency_end),
+    ("when did % end his presidency".split(), incumbency_end),
+    ("what year did % leave office".split(), incumbency_end_year),
+    ("when year did % end his presidency".split(), incumbency_end_year),
+    ("what number president is %".split(), number),
+    ("what number president was %".split(), number),
+    ("which number president is %".split(), number),
+    ("which number president was %".split(), number),
+
     (["bye"], bye_action),
 ]
 
@@ -172,10 +286,15 @@ def search_pa_list(src: List[str]) -> List[str]:
         a list of answers. Will be ["I don't understand"] if it finds no matches and
         ["No answers"] if it finds a match but no answers
     """
+    global previous_name
     for pat, act in pa_list:
-        mat = match(pat, src)
+        mat:List[str] = match(pat, src)
+        if mat is not None and "he" in mat or "they" in mat and previous_name is not None:
+            answer = act([previous_name])
+            return answer if answer else ["No answers"]
         if mat is not None:
             answer = act(mat)
+            previous_name = mat[0]
             return answer if answer else ["No answers"]
 
     return ["I don't understand"]
@@ -184,7 +303,7 @@ def search_pa_list(src: List[str]) -> List[str]:
 def query_loop() -> None:
     """The simple query loop. The try/except structure is to catch Ctrl-C or Ctrl-D
     characters and exit gracefully"""
-    print("Welcome to the movie database!\n")
+    print("Presidental Information System")
     while True:
         try:
             print()
@@ -200,4 +319,16 @@ def query_loop() -> None:
 
 
 # uncomment the next line once you've implemented everything are ready to try it out
-query_loop()
+# query_loop()
+
+
+# I included a feature to hold context between queries. It simply stores the last entered name in a global variable and calls upon it when the prononu 'he' is used
+#demo:
+
+answers = search_pa_list("when did abraham lincoln take office".replace("?", "").lower().split())
+for ans in answers:
+    print(ans)
+
+answers = search_pa_list("when did he leave office".replace("?", "").lower().split())
+for ans in answers:
+    print(ans)
